9c7cb67b8d86bce3027df2fd46cc507bb6de90bb
FatmaMama <fatma_mama@hotmail.fr>
2021-12-06 23:57:18 +0100

    unhandled promise rejection-uncaught exception errors

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,27 @@ const dotenv = require('dotenv');
 //setting up config file 
 dotenv.config({path: 'backend/config/config.env'});
 
+//Handle uncaught exceptions (for example console.log(x) but x is not defined => error)
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('UNCAUGHT EXCEPTION, Shutting down the server...')
+    process.exit(1)
+})
+
 const app = require('./app.js');
 
 const connectDB = require('./config/database')
 
-
-
+//connect database
 connectDB();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`server running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
 })
 
+//Handle unhandled promise rejections (like the connection of mongoDB)
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION, Shutting down the server...')
+    server.close(() => process.exit(1))
+})
