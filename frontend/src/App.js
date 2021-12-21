922ed209e0d217a445f0ea2bc978ab08b2af85f6
FatmaMama <fatma_mama@hotmail.fr>
2021-12-21 14:54:57 +0100

    loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import './css/main.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layouts/Header';
-import Loader from './components/layouts/Loader';
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header/>
-        <Loader/>
       </div>
     </Router>
   );
