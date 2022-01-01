57b6797b6ee6e74169a9d2478e9afa1ce1144fe7
FatmaMama <fatma_mama@hotmail.fr>
2022-01-01 22:22:10 +0100

    add numOfOrders by status

--- frontend/src/components/admin/orders/OrdersRecap.js
+++ frontend/src/components/admin/orders/OrdersRecap.js
@@ -16,7 +16,7 @@ export default function OrdersRecap() {
     const { orders, error } = useSelector(state => state.orders);
     const { message, messageType } = useSelector(state => state.notify)
 
-    const nonTreatedOrders = orders && orders.find(order => order.orderStatus === "non traitée");
+    const nonTreatedOrders = orders && orders.find(order => order.orderStatus === "non-traitée");
     const shippedOrders = orders && orders.find(order => order.orderStatus === "expédiée");
     const deliveredOrders = orders && orders.find(order => order.orderStatus === "livrée");
     const returnedOrders = orders && orders.find(order => order.orderStatus === "retournée");
@@ -45,12 +45,15 @@ export default function OrdersRecap() {
             <div className="col-12 col-md-10 px-5 dashboard">
                 <h1 className="text-uppercase my-5" >Récapitulation des commandes</h1>
                 {error && <Alert  message={message} messageType={messageType}/>}
-                <h3>{console.log(nonTreatedOrders)}</h3>
+               
                 <div className='row'>
                     <div className="col-lg-6 col-12 mb-3 mt-3">
                         <div className="card text-white bg-info o-hidden h-100">
                             <div className="card-body">
-                                <div className="text-center"><i className="fas fa-question-circle me-3"></i>Commandes Non Traitées</div>
+                                <div className="text-center">
+                                    <i className="fas fa-question-circle me-3"></i>
+                                    Commandes Non Traitées {`(${nonTreatedOrders ? nonTreatedOrders.numOfOrders : 0})`}
+                                </div>
                             </div>
                             <Link className="card-footer text-white clearfix small z-1" to="/admin/orders/status/non-traitée">
                                 <span className="float-start">Voir Les Details</span>
@@ -64,7 +67,10 @@ export default function OrdersRecap() {
                     <div className="col-lg-6 col-12 mb-3 mt-3">
                         <div className="card text-white bg-danger o-hidden h-100">
                             <div className="card-body">
-                                <div className="text-center"><i className="fas fa-shipping-fast me-3"></i>Commandes Expédiées</div>
+                                <div className="text-center">
+                                    <i className="fas fa-shipping-fast me-3"></i>
+                                    Commandes Expédiées {`(${shippedOrders ? shippedOrders.numOfOrders : 0})`}
+                                </div>
                             </div>
                             
                             <Link className="card-footer text-white clearfix small z-1" to="/admin/orders/status/expédiée">
@@ -81,7 +87,10 @@ export default function OrdersRecap() {
                     <div className="col-lg-6 col-12 mb-3 mt-3">
                         <div className="card text-white bg-success o-hidden h-100">
                             <div className="card-body">
-                                <div className="text-center"><i className="fas fa-money-bill-alt me-3"></i>Commandes Livrées</div>
+                                <div className="text-center">
+                                    <i className="fas fa-money-bill-alt me-3"></i>
+                                    Commandes Livrées {`(${deliveredOrders ? deliveredOrders.numOfOrders : 0})`}
+                                </div>
                             </div>
                             <Link className="card-footer text-white clearfix small z-1" to="/admin/orders/status/livrée">
                                 <span className="float-start">Voir Les Details</span>
@@ -95,7 +104,10 @@ export default function OrdersRecap() {
                     <div className="col-lg-6 col-12 mb-3 mt-3">
                         <div className="card text-white bg-warning o-hidden h-100">
                             <div className="card-body">
-                                <div className="text-center"><i className="fas fa-angry me-3"></i>Commandes Retournées</div>
+                                <div className="text-center">
+                                    <i className="fas fa-angry me-3"></i>
+                                    Commandes Retournées {`(${returnedOrders ? returnedOrders.numOfOrders : 0})`}
+                                </div>
                             </div>
                             <Link className="card-footer text-white clearfix small z-1" to="/admin/orders/status/retournée">
                                 <span className="float-start">Voir Les Details</span>
